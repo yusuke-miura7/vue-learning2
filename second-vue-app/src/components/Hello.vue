070b8f0c3262333b313f593ea4ed6460f7b3510a
yusuke-miura <miura2000T@outlook.jp>
2023-04-28 09:22:52 +0900

    ref,reactive関数を利用したボタンの追加

--- second-vue-app/src/components/Hello.vue
+++ second-vue-app/src/components/Hello.vue
@@ -4,13 +4,23 @@ const count = ref(0);
 const state = reactive({
     count: 1,
 });
+
+const addRefCount = () =>{
+    count.value++;
+}
+
+const addReactiveCount = () =>{
+    state.count++;
+}
 </script>
 
 <template>
     <div>
     <h2>初めてのコンポーネント</h2>
     <p>Ref Count:{{ count }}</p>    
-    <p>Reactive Count:{{ state.count }}</p>    
+    <p>Reactive Count:{{ state.count }}</p>
+    <button @click="addRefCount">Ref Count++</button>    
+    <button @click="addReactiveCount">Reactive Count++</button>    
 
     </div>
 </template>
