95fc3cb8782a299d76cf6c87e869908b10455b96
yusuke-miura <miura2000T@outlook.jp>
2023-04-28 10:34:13 +0900

    引数がreactiveな変数の場合の適用

--- second-vue-app/src/App.vue
+++ second-vue-app/src/App.vue
@@ -1,11 +1,21 @@
 <script setup>
 import Hello from './components/Hello.vue';
+import { ref } from 'vue';
 
+const name = ref('Mike');
+const changeName = () =>{
+  name.value = 'Bob';
+}
 </script>
 
 <template>
   <Hello message="Propsの使い方" name="BEGINNER" />
+  <br>
   <Hello message="defineProps関数を利用" :price="1000" :isAdmin="true"/>
+  <br>
+  <Hello message="reactiveな変数を渡す" :name="name" />
+  <button @click="changeName">Change Name</button>
+
 </template>
 
 <style scoped></style>
