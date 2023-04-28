ed04ccbe7690ee3e64e4f05042ea78a056880700
yusuke-miura <miura2000T@outlook.jp>
2023-04-28 09:06:59 +0900

    ref,reactive関数の実装

---
+++ second-vue-app/src/components/Hello.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {ref,reactive} from 'vue';
+const count = ref(0);
+const state = reactive({
+    count: 1,
+});
+</script>
+
+<template>
+    <div>
+    <h2>初めてのコンポーネント</h2>
+    <p>Ref Count:{{ count }}</p>    
+    <p>Reactive Count:{{ state.count }}</p>    
+
+    </div>
+</template>
